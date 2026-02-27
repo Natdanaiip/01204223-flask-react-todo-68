5cbc840b04d1143c0fbfb43d7773af074cd55365
Natdanai PHONGPENSRI <natdanaip129w@gmail.com>
2026-02-27 14:13:13 +0700

    fix api/todos/ GET

--- backend/main.py
+++ backend/main.py
@@ -32,6 +32,7 @@ todo_list = [
 ]
 
 @app.route('/api/todos/', methods=['GET'])
+@jwt_required()
 def get_todos():
     todos = TodoItem.query.all()
     return jsonify([todo.to_dict() for todo in todos])
@@ -111,8 +112,3 @@ def login():
     access_token = create_access_token(identity=user.username)
     return jsonify(access_token=access_token)
 
-@app.route('/api/todos/', methods=['GET'])
-@jwt_required()
-def get_todos():
-    todos = TodoItem.query.all()
-    return jsonify([todo.to_dict() for todo in todos])
